3195846a25926cda1b07a8c40331582512eb2f9e
MaryiaTrutsko <maryia.trutsko@gmail.com>
2025-08-05 02:00:38 +0300

    hw4 is done

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -2,7 +2,27 @@
 
 (НЕ ОБЯЗАТЕЛЬНО)
 
-Преобразовать Task 2 таким образом, чтобы значение НАПРИМЕР '2' (т.е. ЛЮБАЯ строка в которой лежат ТОЛЬКО ЦИФРЫ) пропускалось, 
+Преобразовать Task 2 таким образом, чтобы значение НАПРИМЕР '2' 
+(т.е. ЛЮБАЯ строка в которой лежат ТОЛЬКО ЦИФРЫ) пропускалось, 
   преобразовываясь в number
 
 */
+const minAge = 18;
+const maxAge = 60;
+let age = "gf";
+
+let ageNumber = Number(age);
+
+if (isNaN(ageNumber)) {
+  console.log("Incorrect data type");
+} else {
+    if (ageNumber < minAge) {
+      console.log("You don't have access cause your age is " + ageNumber + " It's less then " + minAge);
+  } else if (ageNumber >= minAge && ageNumber < maxAge) {
+      console.log("Welcome !");
+  } else if (ageNumber > maxAge) {
+      console.log("Keep calm and look Culture channel");
+  } else {
+      console.log("Technical work");
+  }
+}
